8cf70038e09692efb4b5ce80ca0c8b11a9ea5da2
Farhan Tanvir <farhantanvir65@gmail.com>
2021-02-27 08:03:44 +0900

    add counter screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import CounterScreen from "./screen/counter-screen";
+// import CounterScreen from "./screen/counter-screen";
 class App extends React.Component {
   render() {
     return <CounterScreen />;
